69b5c8ac191adbc4c1ecb145d972107c4237027f
SAYAKOV <bairsayak2009@gmail.com>
2024-03-06 20:51:25 +0600

    kirill lox

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Main());
+}
+class Main extends StatefulWidget {
+  const Main({super.key});
+
+  @override
+  State<Main> createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('КИРИЛЛ ЛОООХ'),
+        ),
+      ),
+    );
+  }
+}
